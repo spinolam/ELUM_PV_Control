72081889bf1d399627a5abc4b67311595d8a3718
Monica Spinola Felix <monicaspix@gmail.com>
2025-01-28 00:11:00 +0100

    updt logic reponse to prioritize pv

--- case_study_2.py
+++ case_study_2.py
@@ -9,34 +9,29 @@ class PVBESSController(PIController):
         super().__init__(kp, ki, delta_t)
         self.command_pv=0
         self.command_bess=0
-        
-    def calculate_max_pv_power(self, pcc_power_out,max_grid_allowed,nominal_pv_power,max_pv_power_lim=np.inf):
-        # Find new max allowed pv power with integral corrections
-        new_max_pv_power = nominal_pv_power + self.integral_action(max_grid_allowed,pcc_power_out)
-        # Saturate allowed pv power nominal value <=PV_max <= max_value_possible
-        new_max_pv_power=max(nominal_pv_power, min(new_max_pv_power, max_pv_power_lim)) #uncomment this line to use saturation
-        return  new_max_pv_power
+
     
-    
-    def calculate_bess_setpoint(self, pcc_power_out,max_grid_allowed,nominal_bess_power):
-        # Find new bess set point with integral corrections
-        bess_setpoint =  self.integral_action(max_grid_allowed,pcc_power_out)
-        # Saturate power setpoint <=PV_max <= max_value_possible
-        bess_setpoint = max(-nominal_bess_power, min(bess_setpoint, nominal_bess_power))
-        return  bess_setpoint
-    
-    def calculate_controller_output(self,pcc_power_out,pv_power_out,bess_power_out, bess_soc,max_grid_allowed,nominal_pv_power,nominal_bess_power):
-        grid_allowed_error=max_grid_allowed-pcc_power_out
-        soc_min=5; # Minimum SOC for BESS
-        soc_max=95; # Maximum SOC for BESS
+    def calculate_controller_output(self,pcc_power_out,pv_power_out,bess_power_out, bess_soc,max_grid_allowed,nominal_pv_power,nominal_bess_power,soc_min=5 ,soc_max=95,max_pv_power_lim=np.inf):
+
+        load_power_observations=pcc_power_out-pv_power_out-bess_power_out
         # Calculate max PV power
-        if grid_allowed_error >0 or bess_soc>=soc_min or np.abs(bess_power_out)>=nominal_bess_power: # Prioritize grid injection if possible
-            new_max_pv_power=self.calculate_max_pv_power(pcc_power_out,max_grid_allowed,nominal_pv_power) 
-        else: # BESS to be used
-            new_max_pv_power=self.command_pv
-    
-        # Calcularte BESS power setpoint
-        bess_setpoint =  self.integral_action(max_grid_allowed,pcc_power_out)
+
+        if pcc_power_out<max_grid_allowed: # Prioritize grid injection if power excess is below grid limit
+            new_max_pv_power=nominal_pv_power + self.integral_action(max_grid_allowed,pcc_power_out)
+            if pv_power_out +load_power_observations<0:
+                bess_setpoint = -(pv_power_out+load_power_observations) # Discharge BESS to cover load
+            else:
+                bess_setpoint = self.command_bess
+            
+        else: # BESS to be charged if power excess grid limits
+            # Calculate BESS power setpoint
+            bess_setpoint = self.integral_action(max_grid_allowed,pcc_power_out)
+            if bess_soc>=soc_max or np.abs(bess_setpoint)>=nominal_bess_power: # Check if possible to charge
+                new_max_pv_power = nominal_pv_power + self.integral_action(max_grid_allowed,pcc_power_out)
+                bess_setpoint = max(-nominal_bess_power, min(bess_setpoint, nominal_bess_power)) 
+            else:
+                new_max_pv_power = self.command_pv
+
         if bess_setpoint >0 and bess_soc<=soc_min: # Check if possible to discharge
             bess_setpoint=0
             self.reset_integral()
